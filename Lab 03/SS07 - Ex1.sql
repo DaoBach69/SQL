adfff2777e7b4355f297342e4713e13b45b48329
DaoBach69 <65002910+DaoBach69@users.noreply.github.com>
2020-08-02 21:26:31 +0700

    Lab 03
    SS 07 - 08

---
+++ Lab 03/SS07 - Ex1.sql
@@ -0,0 +1,13 @@
+
+SELECT * FROM Person.Address
+
+SELECT DISTINCT (City) FROM Person.Address
+
+SELECT TOP 10 * FROM Person.Address
+
+SELECT TOP 25 PERCENT * FROM Person.Address
+
+SELECT AVG(Rate) FROM HumanResources.EmployeePayHistory
+
+SELECT COUNT(EmployeeID) AS TitleCount FROM HumanResources.Employee
+
